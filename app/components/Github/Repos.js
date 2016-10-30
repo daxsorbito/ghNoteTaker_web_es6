d42b5f0b622f05e3caa28b97bcc2b39b265bbb0a
Dax Sorbito <daxsorbito@gmail.com>
2016-10-30 19:54:44 +0800

    feat: changed to stateless functional components

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,25 +1,44 @@
 import React from 'react'
 
-class Repos extends React.Component {
-  render() {
-    const repos = this.props.repos.map((repo, index) => {
-      return (
-        <li className="list-group-item" key={index}>
+const Repos = ({repos}) => {
+  const repoItems = repos.map((repo, index) => {
+    return (
+      <li className="list-group-item" key={index}>
         {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
         {repo.description && <p>{repo.description}</p>}
-        </li>
-      )
-    })
-    return (
-      <div>
-        <h3> User Repos </h3>
-        <ul className="list-group">
-          {repos}
-        </ul>
-      </div>
+      </li>
     )
-  }
+  })
+  return (
+    <div>
+      <h3> User Repos </h3>
+      <ul className="list-group">
+        {repoItems}
+      </ul>
+    </div>
+  )
 }
+
+// class Repos extends React.Component {
+//   render() {
+//     const repos = this.props.repos.map((repo, index) => {
+//       return (
+//         <li className="list-group-item" key={index}>
+//         {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+//         {repo.description && <p>{repo.description}</p>}
+//         </li>
+//       )
+//     })
+//     return (
+//       <div>
+//         <h3> User Repos </h3>
+//         <ul className="list-group">
+//           {repos}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
 
 Repos.propTypes = {
   username: React.PropTypes.string.isRequired,
--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
-class NotesList extends React.Component {
-  render() {
-    const { notes } = this.props
-    const notesList = notes.map((note, index) => {
-      return <li className="list-group-item" key={index}>{note}</li>
-    })
+const NotesList = ({notes}) => {
+  const notesList = notes.map((note, index) => {
+    return <li className="list-group-item" key={index}>{note}</li>
+  })
 
-    return (
-      <ul className="list-group">
-      { notesList }
-      </ul>
-    )
-  }
+  return (
+    <ul className="list-group">
+    { notesList }
+    </ul>
+  )
 }
+
+// class NotesList extends React.Component {
+//   render() {
+//     const { notes } = this.props
+//     const notesList = notes.map((note, index) => {
+//       return <li className="list-group-item" key={index}>{note}</li>
+//     })
+
+//     return (
+//       <ul className="list-group">
+//       { notesList }
+//       </ul>
+//     )
+//   }
+// }
 
 export default NotesList
