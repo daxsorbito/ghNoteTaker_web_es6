1387f18e2bdd0ff3a65c9c0354bf62989bb5c005
Dax Sorbito <daxsorbito@gmail.com>
2016-10-30 11:07:29 +0800

    feat: changed AddNote

--- app/components/Notes/AddNote.js
+++ app/components/Notes/AddNote.js
@@ -1,28 +1,32 @@
 import React from 'react'
 
-const AddNote = React.createClass({
-  propTypes: {
-    username: React.PropTypes.string.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-  setRef: function(ref) {
-    this.note = ref
-  },
-  handleSubmit: function() {
+class AddNote extends React.Component {
+  handleSubmit() {
     const newNote = this.note.value
     this.note.value = ''
     this.props.addNote(newNote)
-  },
-  render: function(){
+  }
+
+  setRef(ref) {
+    this.note = ref
+  }
+
+  render() {
     return (
       <div className="input-group">
-        <input type="text" className="form-control" placeholder="Add New Note" ref={this.setRef}/>
+        <input type="text" className="form-control" placeholder="Add New Note" ref={(ref) => this.setRef(ref)}/>
         <span className="input-group-btn">
-          <button className="btn btn-default" type="button" onClick={this.handleSubmit}>Submit</button>
+          <button className="btn btn-default" type="button" onClick={this.handleSubmit.bind(this)}>Submit</button>
         </span>
       </div>
     )
   }
-})
+}
 
-module.exports = AddNote
+AddNote.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  addNote: React.PropTypes.func.isRequired
+}
+
+export default AddNote
+
