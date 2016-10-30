8f6092f9cc07daabbada6ca083d1383a3d47a3c1
Dax Sorbito <daxsorbito@gmail.com>
2016-10-30 11:13:48 +0800

    feat: convert Notes to es6

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -2,21 +2,22 @@ import React from 'react'
 import NotesList from './NotesList'
 import AddNote from './AddNote'
 
-const Notes = React.createClass({
-  propTypes: {
-    username: React.PropTypes.string.isRequired,
-    notes: React.PropTypes.array.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-  render: function() {
-    return (
-      <div>
-        <h3> Notes for {this.props.username} </h3>
-        <AddNote username={this.props.username} addNote={this.props.addNote} />
-        <NotesList notes={this.props.notes} />
-      </div>
-    )
-  }
-})
+class Notes extends React.Component {
+  render () {
+    <div>
+      <h3> Notes for {this.props.username} </h3>
+      <AddNote username={this.props.username} addNote={this.props.addNote} />
+      <NotesList notes={this.props.notes} />
+    </div>
 
-module.exports = Notes
+  }
+}
+
+Notes.proptTypes =  {
+  username: React.PropTypes.string.isRequired,
+  notes: React.PropTypes.array.isRequired,
+  addNote: React.PropTypes.func.isRequired
+}
+
+export default Notes
+
